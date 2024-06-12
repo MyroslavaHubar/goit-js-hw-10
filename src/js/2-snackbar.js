64a14436b3aa94d4f219df711cbab9e93d9b3143
Myroslava Hubar <miroslavagubar777@gmail.com>
2024-06-12 23:18:31 +0200

    update 2 task file

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -4,6 +4,51 @@ import 'izitoast/dist/css/iziToast.min.css';
 const formElem = document.querySelector('.form');
 const inputElem = formElem.querySelector('input');
 
-console.log(inputElem);
+// console.log(inputElem);
 
 inputElem.classList.add('input-js');
+
+formElem.addEventListener('submit', promiseGenerator);
+
+function promiseGenerator(e) {
+  e.preventDefault();
+
+  const formData = new FormData(this);
+  const delay = formData.get('delay');
+  const state = formData.get('state');
+
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (state === 'fulfilled') {
+        resolve(delay);
+      } else {
+        reject(delay);
+      }
+    }, delay);
+  });
+
+  promise
+    .then(delay => {
+      iziToast.success({
+        title: '',
+        message: `✅ Fulfilled promise in ${delay}ms`,
+        messageLineHeight: '1.5',
+        messageSize: '16',
+        messageColor: '#fff',
+        position: 'topRight',
+        backgroundColor: '#59a10d',
+      });
+    })
+    .catch(delay => {
+      iziToast.success({
+        title: '',
+        message: `❌ Rejected promise in ${delay}ms`,
+        messageLineHeight: '1.5',
+        messageSize: '16',
+        messageColor: '#fff',
+        position: 'topRight',
+        backgroundColor: '#ef4040',
+      });
+    });
+  console.log(promise);
+}
